42d3b297bdc3e5a114907b7808b87bb3883ed9de
fjy202 <40914915+fjy202@users.noreply.github.com>
2018-07-20 13:17:25 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,5 +2,17 @@ import time
 import random
 i=0
 door=30
-w
-G =input（"走吗?(y/n)")
+while i>door:
+  G =input（"走吗?(y/n),q=quit")
+  if G="q":
+    exit()
+  elif g=="y":
+    i=+randint(1,6)
+    if i==door:
+      print("到了！")
+      exit()
+    elif i>door:
+      break()
+    else:
+      time.sleep(0)
+print("坏人发现了你……")
