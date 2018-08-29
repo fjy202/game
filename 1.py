063219381753bd91d1e12353fd14c42256429a29
fjy202 <40914915+fjy202@users.noreply.github.com>
2018-08-29 10:54:53 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,21 @@
 import time
 import random
+import pygame as pg
+screen=pg.display.set_mode([800,600],pg.NOFRAME)
+a='探秘'
+pg.display.set_caption('探秘')
 i=0
 door=30
-while i<door:
-  G =input（"走吗?(y/n),q=quit")
-  if G="q":
-    exit()
-  elif g=="y":
-    i+=randint(1,6)
-    if i==door:
-      print("到了！")
-      exit()
-    elif i>door:
-      break()
-    else:
-      time.sleep(0)
-print("坏人发现了你……")
+while True:
+    for e in pg.event.get():
+        if e.type==pg.QUIT:
+            pygame.quit()
+        if e.type===pg.KEYDOWN:
+            if e.key==pg.K_UP:
+                i+=random.randint(1,9)
+                if i>door:
+                    pg.display.set_caption('探秘(你死了)')
+                    pygame.quit()
+                    exit()
+            pg.draw.circle(screen,(0,255,0),(10,i),7)
+            
